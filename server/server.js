f881228e41d156b91586cec80288dc0b6987325c
Hazzor <hesterliong@gmail.com>
2017-07-23 09:11:50 +0800

    Emit newMessage and createMessage

--- server/server.js
+++ server/server.js
@@ -29,6 +29,11 @@ io.on('connection' , (socket)=>{
 
     socket.on('createMessage', (message)=>{
         console.log('new message from client',message);
+        io.emit('newMessage', {
+            from : message.from,
+            text : message.text,
+            createdAt : new Date().getTime()
+        })
     });
 
     socket.on('createEmail', (newEmail)=>{
