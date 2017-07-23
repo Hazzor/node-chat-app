81286f817db96180142e493d4c5e30a88165a340
Hazzor <hesterliong@gmail.com>
2017-07-23 09:43:39 +0800

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -11,30 +11,48 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-//listen to client conn and create web socket to individual client
+//listen to any conn and create web socket to individual client
 io.on('connection' , (socket)=>{
     console.log('New user connected');
 
-    socket.emit('newEmail', {
-        from : 'hester@gmail.com',
-        text : 'come here fast',
-        createdAt : 123
-    });
-
     socket.emit('newMessage', {
-        from : 'hester',
-        text : 'come here fast.. play badminton jom!',
-        createdAt : new Date().getTime().toString()
+        from : 'admin',
+        text : 'welcome to the chat app'
     });
+    socket.broadcast.emit('newMessage', {
+        text : 'New user joined',
+        from : 'admin',
+        createdAt : new Date().getTime()
+    })
 
-    socket.on('createMessage', (message)=>{
-        console.log('new message from client',message);
-        io.emit('newMessage', {
-            from : message.from,
-            text : message.text,
-            createdAt : new Date().getTime()
-        })
-    });
+    // socket.emit('newEmail', {
+    //     from : 'hester@gmail.com',
+    //     text : 'come here fast',
+    //     createdAt : 123
+    // });
+
+    // socket.emit('newMessage', {
+    //     from : 'hester',
+    //     text : 'come here fast.. play badminton jom!',
+    //     createdAt : new Date().getTime().toString()
+    // });
+
+//     socket.on('createMessage', (message)=>{
+//         console.log('new message from client',message);
+// // io broadcast to others
+//         // io.emit('newMessage', {
+//         //     from : message.from,
+//         //     text : message.text,
+//         //     createdAt : new Date().getTime()
+//         // });
+
+// //emit to other people but not you
+//         // socket.broadcast.emit('newMessage', {
+//         //     from : message.from,
+//         //     text : message.text,
+//         //     createdAt : new Date().getTime()
+//         // });
+//     });
 
     socket.on('createEmail', (newEmail)=>{
         console.log('new email from client', newEmail);
@@ -44,9 +62,6 @@ io.on('connection' , (socket)=>{
         console.log('User was disconnected')
     });
 });
-
-
-
 
 //use nodejs http
 server.listen(port, ()=>{
