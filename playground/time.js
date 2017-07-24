6a0476b9d2a709cafa1e69d6129ebd53dbed3e6b
Hazzor <hesterliong@gmail.com>
2017-07-24 21:43:53 +0800

    Add moustache.js for mesej template

--- playground/time.js
+++ playground/time.js
@@ -1,9 +1,11 @@
 var moment = require('moment');
 
-var date = new moment();
-date.subtract(10, 'year').add(1, 'month');
-console.log(date.format('Do MMM YYYY'));
+// var date = new moment();
+// date.subtract(10, 'year').add(1, 'month');
+// console.log(date.format('Do MMM YYYY'));
 
-var date1 = moment();
+// var date1 = moment();
+// console.log(date1.format('h:mm a'));
 
-console.log(date1.format('h:mm a'));
+var timeStamp = moment().valueOf();
+console.log(timeStamp);
--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
+// use function instead of template string
+
 // create connection instance, not always on(client)
 var socket = io();
 // socket.io._reconnection = false;
@@ -17,12 +19,24 @@ socket.on('connect', function (){
 });
 
 socket.on('newLocationMessage', function (message) {
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My current location</a>');
-    li.text(`${message.from}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
+    // var formattedTime = moment(message.createdAt).format('h:mm a');
+    // var li = jQuery('<li></li>');
+    // var a = jQuery('<a target="_blank">My current location</a>');
+    // li.text(`${message.from} ${formattedTime} : `);
+    // a.attr('href', message.url);
+    // li.append(a);
+    // jQuery('#messages').append(li);
+
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template, {
+        url : message.url,
+        from : message.from,
+        createdAt : formattedTime
+    });
+
+    jQuery('#messages').append(html);
+
 
 });
 
@@ -46,12 +60,23 @@ socket.on('newEmail', function(email) {
 });
 
 socket.on('newMessage', function(message) {
-    console.log('New message', message);
-    //create new jquery object
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} : ${message.text}`);
 
-    jQuery('#messages').append(li);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template, {
+        text : message.text,
+        from : message.from,
+        createdAt : formattedTime
+    });
+
+    jQuery('#messages').append(html);
+
+    // console.log('New message', message);
+    //create new jquery object
+    // var time = jQuery('<h1 style="color:green;text-align:right">'+formattedTime+'</h1>');
+    // var li = jQuery('<li></li>');
+    // li.text(`${message.from} : ${message.text}`);
+    // jQuery('#messages').append(li);
 });
 
 // socket.emit('createMessage', {
